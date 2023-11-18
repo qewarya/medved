b8dad733b8f5f12689751c029ee5212bba10646b
qewarya <zxkaraff@gmail.com>
2023-11-18 20:24:53 +0500

    +mob

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,27 +1,20 @@
 extends CharacterBody2D
 
-const SPEED = 200
+var speed = 200.0
+var chase = false 
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
-	var motion = Vector2.ZERO
-	var player = get_parent().get_node("Player")
-	var player_position = player.global_position
-	var enemy_position = global_position
-	var direction = player_position - enemy_position
-	direction = direction.normalized()
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	var player = $"../../Player/Player"
+	var direction = (player.position - self.position).normalized()
+	if chase == true:
+		velocity.x = speed  * direction.x
+	
+	move_and_slide()
 
-
-
-	if Input.is_action_pressed("ui_right"):
-		motion.x += SPEED
-
-	if Input.is_action_pressed("ui_left"):
-		motion.x -= SPEED
-
-	if Input.is_action_pressed("ui_down"):
-		motion.y += SPEED
-
-	if Input.is_action_pressed("ui_up"):
-		motion.y -= SPEED
-
-	motion = move_and_slide()
+func _on_detector_body_entered(body):
+	if body.name == "Player":
+		chase = true
+	
--- mob.gd
+++ mob.gd
@@ -1,27 +1,2 @@
 extends CharacterBody2D
 
-const SPEED = 200
-
-func _physics_process(delta):
-	var motion = Vector2.ZERO
-	var player = get_parent().get_node("Player")
-	var player_position = player.global_position
-	var enemy_position = global_position
-	var direction = player_position - enemy_position
-	direction = direction.normalized()
-
-
-
-	if Input.is_action_pressed("ui_right"):
-		motion.x += SPEED
-
-	if Input.is_action_pressed("ui_left"):
-		motion.x -= SPEED
-
-	if Input.is_action_pressed("ui_down"):
-		motion.y += SPEED
-
-	if Input.is_action_pressed("ui_up"):
-		motion.y -= SPEED
-
-	motion = move_and_slide()
