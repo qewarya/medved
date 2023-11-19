8c06efc75bf1216b09ffef3b4060837e807b513b
qewarya <zxkaraff@gmail.com>
2023-11-19 14:37:52 +0500

    swsw

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,20 +1,10 @@
 extends CharacterBody2D
 
-var speed = 200.0
+var speed = 0.0
 var chase = false 
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	var player = $"../../Player/Player"
-	var direction = (player.position - self.position).normalized()
-	if chase == true:
-		velocity.x = speed  * direction.x
-	
-	move_and_slide()
-
-func _on_detector_body_entered(body):
-	if body.name == "Player":
-		chase = true
 	
